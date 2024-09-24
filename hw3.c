2f26366865aaac019f630c7a67af94462d9c9540
kairos1228 <88337917+kairos1228@users.noreply.github.com>
2024-09-24 17:36:29 +0900

    C프로그래밍 4주차 과제

---
+++ hw3.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int n,m = 0, space = 4;
+
+	for (int space = 4; space >= 0; space--) {
+		for (int m = 0; m < space; m++) {
+			printf(" ");
+		}
+		for (int n = 0; n < m; n++) {
+				printf("**");
+		}
+		printf("*\n");
+		}
+	return 0;
+}
