73b4f0c33f8d054ca1db633c7a5a1c76bbeeae50
kairos1228 <88337917+kairos1228@users.noreply.github.com>
2024-10-15 15:11:57 +0900

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -10,6 +10,8 @@ int main(void)
 	printf("%d & %d = %d \n", num1, num2, num1&num2);
 	printf("%d | %d = %d \n", num1, num2, num1|num2);
 	printf("%d ^ %d = %d", num1, num2, num1^num2);
+	printf("pow(%d, %d) = %d\n", num1, num2, (int)pow(num1, num2)); 
+	// a의 n승: a^n이 아닌 #include <math.h> 이후 pow(a,n) 이용
 
 	return 0;
 }
