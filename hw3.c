df4ab3f4a792b324b431169aa7e57952a3af7a14
kairos1228 <88337917+kairos1228@users.noreply.github.com>
2024-10-15 15:23:46 +0900

    Update hw3.c

--- hw3.c
+++ hw3.c
@@ -2,17 +2,18 @@
 #include <stdio.h>
 int main(void)
 {
-	int n,m = 0, space = 4;
+	int n, m = 0;
+	const int space = 10; // const : 상수가 한번 설정되면 고치지 못하게 함
 
-	for (int space = 4; space >= 0; space--) {
+	for (space; space >= 0; space--) {
 		for (int m = 0; m < space; m++) {
 			printf(" ");
 		}
 		for (int n = 0; n < m; n++) {
-				printf("**");
+			printf("**");
 		}
-		m += 1
+		m += 1;
 		printf("*\n");
-		}
+	}
 	return 0;
 }
