11c1eb1af7604868b50afa5ba8936a70d1f8fff7
kairos1228 <88337917+kairos1228@users.noreply.github.com>
2024-10-15 15:24:51 +0900

    Update hw3.c

--- hw3.c
+++ hw3.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 	int n, m = 0;
-	const int space = 10; // const : 상수가 한번 설정되면 고치지 못하게 함
+	int space = 10;
 
 	for (space; space >= 0; space--) {
 		for (int m = 0; m < space; m++) {
